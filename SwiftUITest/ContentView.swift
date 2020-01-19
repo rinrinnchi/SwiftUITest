8c166e9bb1e9eb4202aa74fff099f790d159837d
Bao Linh <masusapu@gmail.com>
2020-01-19 11:59:41 +0900

    1st commit

---
+++ SwiftUITest/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  SwiftUITest
+//
+//  Created by Bao Linh on 2020/01/19.
+//  Copyright © 2020 Bao Linh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
