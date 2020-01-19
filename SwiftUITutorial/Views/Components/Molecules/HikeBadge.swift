d0f28678dbcef36979a053c4d1892ec2180f7f5f
Bao Linh <masusapu@gmail.com>
2020-01-20 01:06:27 +0900

    add Profile

---
+++ SwiftUITutorial/Views/Components/Molecules/HikeBadge.swift
@@ -0,0 +1,31 @@
+//
+//  HikeBadge.swift
+//  SwiftUITutorial
+//
+//  Created by Bao Linh on 2020/01/20.
+//  Copyright © 2020 Bao Linh. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeBadge: View {
+    var name: String
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Badge()
+                .frame(width: 300, height: 300)
+                .scaleEffect(1.0 / 3.0)
+                .frame(width: 100, height: 100)
+            Text(name)
+                .font(.caption)
+                .accessibility(label: Text("Badge for \(name)."))
+        }
+    }
+}
+
+struct HikeBadge_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeBadge(name: "Preview Testing")
+    }
+}
--- SwiftUITutorial/Views/Pages/Home.swift
+++ SwiftUITutorial/Views/Pages/Home.swift
@@ -21,6 +21,7 @@ struct CategoryHome: View {
     }
     
     @State var showingProfile = false
+    @EnvironmentObject var userData: UserData
     
     var profileButton: some View {
         Button(action: { self.showingProfile.toggle() }) {
@@ -51,7 +52,8 @@ struct CategoryHome: View {
             .navigationBarTitle("Featured")
             .navigationBarItems(trailing: profileButton)
             .sheet(isPresented: $showingProfile) {
-                Text("User Profile")
+                ProfileHost()
+                    .environmentObject(self.userData)
             }
         }
     }
